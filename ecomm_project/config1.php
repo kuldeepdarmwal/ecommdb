ad700dda77f08cff698354f396d6dac2c4626758
Gandhar <gandhark@cybage.com>
2015-05-27 15:15:05 +0530

    aefeqrf

--- ecomm_project/config1.php
+++ ecomm_project/config1.php
@@ -6,9 +6,9 @@
 */
 class Config
  {
-    const HOST     = '172.27.59.54',
-          USER     = 'ecomm',
-          PASSWORD = 'ecomm@123',
+    const HOST     = 'localhost',
+          USER     = 'root',
+          PASSWORD = 'root',
 		
 		HELPER = "Helper.php";
 } 
