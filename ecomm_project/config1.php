a3b8d29918e8b7e24484fb7a79b0857a3143f0be
root <root@localhost.localdomain>
2015-05-27 15:29:57 +0530

    waefrfqef

--- ecomm_project/config1.php
+++ ecomm_project/config1.php
@@ -6,9 +6,9 @@
 */
 class Config
  {
-    const HOST     = 'localhost',
-          USER     = 'root',
-          PASSWORD = 'root',
+    const HOST     = '172.27.59.54',
+          USER     = 'ecomm',
+          PASSWORD = 'ecomm@123',
 		
 		HELPER = "Helper.php";
 } 
